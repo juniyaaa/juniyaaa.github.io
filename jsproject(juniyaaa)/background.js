b223372ba12ca5f4aabdb141cbbffc51d701fa1a
juniyaaa <125620812+juniyaaa@users.noreply.github.com>
2023-03-06 02:31:27 +0900

    Delete jsproject(juniyaaa) directory

--- jsproject(juniyaaa)/background.js
+++
@@ -1,9 +0,0 @@
-const image = ["1.jpg", "2.jpg" , "3.jpg", "4.jpg"];
-
-const choseImage = image[Math.floor(Math.random()*image.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${choseImage}`;
-
-document.body.appendChild(bgImage);
